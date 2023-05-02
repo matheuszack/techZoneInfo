e15a048337a6b4f0977414c026a4201dec96395f
matheuszack <113534912+matheuszack@users.noreply.github.com>
2023-05-02 20:37:12 -0300

    Delete techZoneInfo-main directory

--- techZoneInfo-main/hamburguer/hamburguepromo.js
+++
@@ -1,9 +0,0 @@
-// Seleciona o ícone de hambúrguer e o menu
-const menuIcon = document.querySelector('.menu-icon');
-const menu = document.querySelector('.menu');
-
-// Adiciona um evento de clique ao ícone de hambúrguer
-menuIcon.addEventListener('click', function() {
-  // Adiciona ou remove a classe 'open' do menu para mostrar ou ocultá-lo
-  menu.classList.toggle('open');
-});
